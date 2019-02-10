8fed8dce14e7beba90128cbe2fcebaf44417627f
Zerovers <Zeromon19941994@mail.ru>
2019-02-10 03:02:56 +0300

    add style

--- game/.eslintrc.js
+++ game/.eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
-    "extends": [
-        "airbnb-base",
-    ],
+    "extends": "airbnb",
+    "parser": "babel-eslint",
     "env": {
-        "browser": true,
-        "node": true,
-        "jquery": true,
-        "jest": true,
+      "browser": true,
+      "node": true,
+      "jest": true
     },
     "rules": {
-        "import/no-cycle": "off",        
+      "consistent-return": "off",
+      "no-console": "off",
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "react/prop-types": [
+        "enabled",
+        { "ignore": "ignore", "customValidators": "customValidator" }
+      ],
+      "react/destructuring-assignment": ["enabled", 'never'],
+      "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
     },
 };
